5bf2b0672ff84369754bc4d662cbe374b86cc7e5
Yale Hartmann <yale.hartmann@uni-bremen.de>
2022-11-03 17:10:02 +0100

    updated nodes and adapted basic test to at least sometimes check stuff

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -36,7 +36,6 @@ class TestProcessing():
 
 
     # essentially: the types are not fully fledged out yet.. :/
-    @pytest.mark.xfail
     def test_all_processable(self):
         # Note: these are very crude tests! 
         # Each node should also be tested separately.
@@ -50,13 +49,19 @@ class TestProcessing():
 
             # heuristic test if should process works with some of the example values provided in the port classes
             # if should process works and returns true, test if process works as well
+            example_values = {}
             for key, val in example_node.ports_in._asdict().items():
-                # originally this would accumulate over each input port, but this would fail, as the example values might not match across multiple ports
-                # i.e. if SelectNode takes data + channels with the same last dimension, this might not be proper with random example values and an errr could be rightfully raised, even though the test should not fail
-                # thus for now we just test single values...
-                # -> likely no processing will happen, but we do already check if the naming is consistent
-                # -> TODO: we could also check consistency within the __subclass__ hook in Node.py...
-                example_values = {key: val.example_values[0]}
+                example_values[key] = val.example_values[0]
                 print(example_values)
-                if example_node._should_process(**example_values):
-                    example_node.process(**example_values)
+                
+                res = None
+                try: 
+                    if example_node._should_process(**example_values):
+                        res = example_node.process(**example_values)
+                except:
+                    pass
+                
+                if res is not None:
+                    for key in res:
+                        if key not in example_node.ports_out:
+                            raise ValueError(f'{key} is no valid out port for {example_node}')
